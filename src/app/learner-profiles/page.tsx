d50fe5bc727d278b4f28a6011418e4fdf86461f9
drew <7977907+notnotdrew@users.noreply.github.com>
2025-12-22 09:17:17 -0700

    Add basic index of learner profiles (#56)
    * Add option to show delete buttons on LearnerProfileCard
    
    * Revert "Add option to show delete buttons on LearnerProfileCard"
    
    This reverts commit 803704d9d5d723ef1920f62b9d99edd6c42040de.
    
    * Add basic index of learner profiles

--- src/app/learner-profiles/page.tsx
+++ src/app/learner-profiles/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import BackButton from "@/components/BackButton";
+import { Divider, Skeleton } from "@heroui/react";
+import { LearnerProfileCard } from "@/lib/learner-profiles";
+import { useLearnerProfiles } from "@demos/_store/useLearnerProfiles";
+
 export default function LearnerProfilesPage() {
-  return <h1>Learner Profiles</h1>;
+  const { data, isLoading, isError, error } = useLearnerProfiles();
+
+  if (isError) {
+    return <p>Error loading courses: {error.message}</p>;
+  }
+
+  return (
+    <div className="max-w-5xl flex flex-col items-center mx-auto p-6 w-full">
+      <BackButton className="self-start mb-4" />
+
+      <div className="w-full">
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-4xl font-bold text-foreground">
+            Learner Profiles
+          </h1>
+        </div>
+
+        <Divider className="mb-8" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 justify-around">
+          {isLoading ? <Skeleton className="h-4 w-1/3 rounded-lg" /> : null}
+          {!isLoading && data ? data.map((learnerProfile) => (
+              <LearnerProfileCard key={learnerProfile.id} learnerProfile={learnerProfile} />
+          )) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/lib/learner-profiles/index.ts
+++ src/lib/learner-profiles/index.ts
@@ -1,4 +1,5 @@
 export type { LearnerProfileRow } from "./models/LearnerProfile";
 export { LearnerProfile } from "./models/LearnerProfile";
+export { default as LearnerProfileCard } from "./components/LearnerProfileCard";
 export { default as LearnerProfileChip } from "./components/LearnerProfileChip";
 export { getLearnerProfiles } from "./services/getLearnerProfiles";
